e7074dfcdaf18bb77c6e09883d54796d03f39589
aftabkhan1234 <aftabkhan@gmail.com>
2023-09-22 11:32:48 +0530

    onProcess3

---
+++ script.js
@@ -0,0 +1,8 @@
+const PlayStaion = document.querySelector(".playsation")
+const XboxSeries = document.querySelector(".Xbox")
+
+PlayStaion.addEventListener("click",()=>{
+    console.log("hello");
+    PlayStaion.style.width = "90%"
+    // XboxSeries.style.
+})
